b462874e997d103686fdc4e9fb963a31589cc9bc
Szantai Gabi <szantai.gabor.19f@szlgbp.hu>
2022-04-23 13:31:12 +0200

    push

--- PROJEKT/settings.py
+++ PROJEKT/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5$tqflbyg%8wtr&gm@k^r=icv27@03n3if5^*ul!^=n2j=w$+k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hellodjangogabi.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
